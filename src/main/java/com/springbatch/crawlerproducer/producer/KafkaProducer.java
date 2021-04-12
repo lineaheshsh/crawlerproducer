7229f720256bd7ba063f103b0f2efb8f6176d2f2
LeeChangHo <lineaheshsh@naver.com>
2021-04-12 23:01:52 +0900

    producer 수정

--- src/main/java/com/springbatch/crawlerproducer/producer/KafkaProducer.java
+++ src/main/java/com/springbatch/crawlerproducer/producer/KafkaProducer.java
@@ -1,7 +1,5 @@
 package com.springbatch.crawlerproducer.producer;
 
-import com.google.gson.Gson;
-import com.springbatch.crawlerproducer.vo.NewsVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -9,16 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducer {
     private static final String TOPIC = "news_p1";
-    /*private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
     public KafkaProducer(KafkaTemplate kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
-    }*/
+    }
 
     public void sendMessage(String message) {
         String msg = message;
-        /*System.out.println(String.format("Produce message : %s", message));*/
-        /*this.kafkaTemplate.send(TOPIC, message);*/
+        System.out.println(String.format("Produce message : %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
     }
 }
--- src/main/java/com/springbatch/crawlerproducer/schedule/ProducerScheduler.java
+++ src/main/java/com/springbatch/crawlerproducer/schedule/ProducerScheduler.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -23,35 +22,19 @@ public class ProducerScheduler {
     @Autowired
     KafkaProducer producer;
 
-    @Scheduled(cron = "10 11 * * * *")
-    public void MySQLtoKafka() {
+    /**
+     * 하루에 한번씩 DB에서 데이터를 읽어와 KAFKA로 데이터를 쏘는 PRODUCER
+     */
+    @Scheduled(cron = "0 0 8 * * *")
+    public void mySQLtoKafka() {
 
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
         String fileName = "last_num.txt";
-        int lastMessageNum = 0;
-        int selectNum = -1;
-
         File file = new File(fileName);
+        int selectNum = getLastNum(file);
+        int lastMessageNum = 0;
 
-        if (file.isFile()) {
-            System.out.println("read files!! " + fileName);
-            try (FileReader rw = new FileReader(file);
-                 BufferedReader br = new BufferedReader(rw);) {
-
-                String readLine = null;
-                while( ( readLine = br.readLine()) != null ) {
-                    selectNum = Integer.parseInt(readLine);
-                }
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("no read files");
-        }
-        List<NewsVO> updateList = new ArrayList<>();
+        List<NewsVO> updateList;
         if ( selectNum != -1 ) {
             System.out.println("inc mode :: " + selectNum);
             updateList = newsMapper.findNewsIdAfter(selectNum);
@@ -62,22 +45,60 @@ public class ProducerScheduler {
 
         System.out.println("[MySQLtoKafka] run : " + LocalDateTime.now() + ", updateList size :: " + updateList.size());
 
-        for (NewsVO newsVO : updateList) {
+        try {
+            for (NewsVO newsVO : updateList) {
+                if (newsVO != null) {
+                    // 카프카 producer
+                    producer.sendMessage(gson.toJson(newsVO));
+                    lastMessageNum = newsVO.getContents_id();
+                }
 
-            if (newsVO != null) {
-                // 카프카 producer
-                producer.sendMessage(gson.toJson(newsVO));
-
-                lastMessageNum = newsVO.getContents_id();
+                if ((lastMessageNum % 1000) == 0) System.out.println("id :: " + lastMessageNum);
             }
-
-            if ((lastMessageNum % 1000) == 0) System.out.println("id :: " + lastMessageNum);
+            System.out.println("last Num :" + lastMessageNum);
+        } catch(Exception e) {
+            System.out.println("Kafka Producer Fail");
+        } finally {
+            // 마지막으로 보낸 메세지의 id값을 파일에 남겨 다음 스케줄에는 id 뒷 번호부터 처리함
+            writeLastNum(file, lastMessageNum);
         }
-        System.out.println("last Num :" + lastMessageNum);
 
-        // 마지막으로 보낸 메세지의 id값을 파일에 남겨 다음 스케줄에는 id 뒷 번호부터 처리함
+    }
 
+    /**
+     * last_num.txt에서 번호를 읽어온다.
+     * @return
+     */
+    private int getLastNum(File file) {
+        int selectNum = -1;
 
+        if (file.isFile()) {
+            try (FileReader rw = new FileReader(file);
+                 BufferedReader br = new BufferedReader(rw);) {
+
+                String readLine = null;
+                while( ( readLine = br.readLine()) != null ) {
+                    selectNum = Integer.parseInt(readLine);
+                }
+
+            } catch (IOException e) {
+                System.out.println("File Read Fail.");
+                selectNum = 0;
+            }
+        } else {
+            System.out.println("File Not Found.");
+            selectNum = 0;
+        }
+
+        return selectNum;
+    }
+
+    /**
+     * kafka로 보낸 마지막 번호를 파일에 남겨 다음 스케줄에서는 해당 값을 기준으로 이후 데이터를 kafka로 보낸다.
+     * @param file
+     * @param lastMessageNum
+     */
+    private void writeLastNum(File file, int lastMessageNum) {
         if ( file.isFile() ) file.delete();
 
         try(FileWriter fw = new FileWriter(file, true)) {
@@ -86,8 +107,7 @@ public class ProducerScheduler {
             fw.flush();
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("File Write Fail.");
         }
-
     }
 }
